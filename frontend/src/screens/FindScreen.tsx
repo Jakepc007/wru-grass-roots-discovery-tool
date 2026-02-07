c2710a1021bf88952d369fa823dce3c375eb04d0
Christo <christo@Christos-MacBook-Air.local>
2026-02-07 14:14:40 +0000

    enter postcode now takes you to find/map screen

--- frontend/src/screens/FindScreen.tsx
+++ frontend/src/screens/FindScreen.tsx
@@ -1,10 +1,20 @@
+import { useSearchParams } from 'react-router-dom'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 
 function FindScreen() {
+  const [searchParams] = useSearchParams()
+  const postcode = searchParams.get('postcode')
+
   return (
     <Box sx={{ height: 'calc(100vh - 64px)', width: '100%' }}>
+      {postcode && (
+        <Typography variant="subtitle1" sx={{ p: 1, textAlign: 'center' }}>
+          Showing results for: <strong>{postcode.toUpperCase()}</strong>
+        </Typography>
+      )}
       <MapContainer
         center={[51.85, -3.5]}
         zoom={8}
