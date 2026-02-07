98546d57e04e385f6205a24e1ffebc05261c5366
Christo <christo@Christos-MacBook-Air.local>
2026-02-07 13:04:49 +0000

    added wru theme colours

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,22 @@
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import theme from './theme'
 import InputLocationScreen from './screens/InputLocationScreen'
 
 function App() {
   return (
-    <>
-      <AppBar position="static">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static" sx={{ borderBottom: '2px solid rgb(209, 138, 0)' }}>
         <Toolbar>
           <Typography variant="h6">WRU Grassroots</Typography>
         </Toolbar>
       </AppBar>
       <InputLocationScreen />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ frontend/src/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: 'rgb(238, 53, 36)',
+    },
+  },
+})
+
+export default theme
